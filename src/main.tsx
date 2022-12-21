65d5754bd135e7855abf1dab99749dbe827dd9ea
unknown <sidhubasho99@gmail.com>
2022-12-22 02:01:36 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import App from "./App";
+import Nav from  "./components/Nav/NavBar";
+import Rarity from "./components/Rarity/Rarity";
+import Text from "./components/Text/Text";
+
+export default function main() {
+  return (
+    <div>
+                <div><Nav/></div>
+                <div><App/></div>
+               
+
+    </div>
+  )
+}
